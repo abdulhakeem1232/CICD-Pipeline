de591469e280639163dd01288ef26d534038fd22
hakeem <hakeemforgit@gmail.com>
2024-04-06 16:39:35 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hi This is Home Page!')
+})
+
+app.listen(3000, () => {
+    console.log('port running');
+})
