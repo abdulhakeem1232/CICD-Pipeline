969de95b0a0aa00c068a675709da7ff2c9f06707
hakeem <hakeemforgit@gmail.com>
2024-04-08 14:17:00 +0530

    failure changes

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('Hi This is Home Page hakeem edited!')
 })
-app.get('/api/product', (req, res) => {
-    res.send("product pahge")
-})
+
 
 app.listen(3000, () => {
     console.log('port running');
