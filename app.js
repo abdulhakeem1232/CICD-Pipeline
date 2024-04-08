58e2294ab24b377037ac642920e156a4de05429a
hakeem <hakeemforgit@gmail.com>
2024-04-08 14:02:10 +0530

    test2

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express()
 app.get('/', (req, res) => {
     res.send('Hi This is Home Page hakeem edited!')
 })
+app.get('/product', (req, res) => {
+    res.send("product pahge")
+})
 
 app.listen(3000, () => {
     console.log('port running');
